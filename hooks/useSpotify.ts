e52a1b9f4b583a99b3855e7524243b8dcf2ac554
Paweł Potempa <pwpotempa@gmail.com>
2022-09-05 15:28:28 +0200

    chore: Change extension of useSpotify hook to .ts

--- hooks/useSpotify.ts
+++ hooks/useSpotify.ts
@@ -3,15 +3,14 @@ import { signIn, useSession } from "next-auth/react";
 import spotifyApi from "../lib/spotify";
 
 const useSpotify = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
 
   useEffect(() => {
     if (session) {
       if (session.error === "RefreshAccessTokenError") {
         signIn();
       }
-
-      spotifyApi.setAccessToken(session.user.setAccessToken);
+      spotifyApi.setAccessToken(session.user.accessToken);
     }
   }, [session]);
   return spotifyApi;
